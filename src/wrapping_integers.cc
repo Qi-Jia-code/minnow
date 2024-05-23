bd2e5b1d5d94b3cdf4aef871c9824beb90801d3f
Qi-Jia-code <3136296488@qq.com>
2024-05-23 10:02:45 +0000

    check2-first

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,35 @@
 #include "wrapping_integers.hh"
+#include "../util/tcp_config.hh"
+#include <tcp_config.hh>
+#include <iostream>
+
 
 using namespace std;
+uint64_t max_int = 4294967296;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return Wrap32{ static_cast<uint32_t>(n) + zero_point.raw_value_ };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint32_t seqno = this->raw_value_ -  zero_point.raw_value_;
+  uint64_t offset = static_cast<uint64_t>(seqno);
+  uint64_t next = (checkpoint / max_int) * max_int + offset;
+  uint64_t left = next - max_int;
+  uint64_t right = next + max_int;
+  if(next > checkpoint) {
+    if(next - checkpoint < checkpoint - left) {
+      return next;
+    }else {
+      return left;
+    }
+  }else {
+    if(right - checkpoint > checkpoint - next) {
+      return next;
+    }else {
+      return right;
+    }
+  }
 }
